11a4b9c3b705cb78ec3690cfdcae920acc856eef
bcosta19 <brpassos@id.uff.br>
2025-05-14 14:55:02 -0300

    corrigindo de novo

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://bcosta19.github.io/desweb.github.io'
+  base: '/'
 })
