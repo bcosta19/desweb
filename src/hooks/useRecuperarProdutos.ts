79573facc1ddbf3789264634749f1184eda9d8be
bcosta19 <brpassos@id.uff.br>
2025-06-21 20:18:39 -0300

    configurando hook para recuperar produtos do back-end, ainda não está sendo utilizado

--- src/hooks/useRecuperarProdutos.ts
+++ src/hooks/useRecuperarProdutos.ts
@@ -1,7 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
 import { Produto } from "../interface/Produto";
 
-const useRecuperarProdutos = () => {
-
-
+async function recuperarProdutos(): Promise<Produto[]> {
+  const response = await fetch("http://localhost:8080/produtos");
+  if (!response.ok) {
+    throw new Error("Erro ao recuperar produtos");
+  }
+  return response.json();
 }
+
+const useRecuperarProdutos = () => {
+  return useQuery<Produto[], Error>({
+    queryKey: ["produtos"],
+    queryFn: recuperarProdutos,
+    refetchOnWindowFocus: false,
+    retry: false,
+  });
+}
+
+export default useRecuperarProdutos;
