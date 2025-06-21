ef13e6a9b84e5ea805ea7487ff79e1dd198504a1
bcosta19 <brpassos@id.uff.br>
2025-06-21 20:23:34 -0300

    adicionando menu para selecionar qual tipo de produto o usuário deseja ver

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,39 @@
+import { NavLink } from "react-router-dom"
+
 const HomePage = () => {
-  return (<div>Home Page</div>)
+  return (
+    <div className="row">
+      <div className="col-lg-2">
+        <nav className="nav nav-pills d-flex flex-column">
+          <h5>Categorias</h5>
+
+          <NavLink className="nav-link" aria-current="page" to="/">
+            Todos
+          </NavLink>
+
+          <NavLink className="nav-link" to="/varas">
+            Varas
+          </NavLink>
+
+          <NavLink className="nav-link" to="/linhas">
+            Linhas
+          </NavLink>
+
+          <NavLink className="nav-link" to="/carretilhas">
+            Carretilhas
+          </NavLink>
+
+          <NavLink className="nav-link" to="/chumbadas">
+            Chumbadas
+          </NavLink>
+        </nav>
+      </div>
+
+    </div>
+
+
+
+  )
 }
 
 export default HomePage
